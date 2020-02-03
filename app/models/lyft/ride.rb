4891e9e1fe9205b9269468f88faa3fbe69d2ca70
placidowang <placidow@gmail.com>
2020-02-03 11:22:10 -0600

    Done.

---
+++ app/models/lyft/ride.rb
@@ -0,0 +1,21 @@
+class Ride
+    attr_accessor :driver, :passenger, :distance
+
+    @@all = []
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.average_distance
+        all.reduce(0.0) {|sum, ride| sum + ride.distance} / @@all.count
+    end
+
+end
